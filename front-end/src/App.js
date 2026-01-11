3e8913d87486a07081174ab485aad6d93f4b7b53
jonelps <jpoueri1@binghamton.edu>
2026-01-11 12:51:25 -0500

    testing front end with component structure for input, edit, and list images, updated App.js that currently just has inputImages component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,31 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
+import React, { useState, Fragment } from 'react';
 import './App.css';
+//components
+import InputImage from './components/InputImages';
 
-function App() {  
+//all this runs when rendered
+function App() {
+
+    /*try {
+      const response = await fetch('http://localhost:3001/api/images', {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (response.ok) {
+        alert('Upload successful');
+      } else {
+        alert('Upload failed');
+      }
+    } catch (error) {
+      console.error('Upload error:', error);
+      alert('Error uploading');
+    }
+  };*/
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <table>
-        <tr>
-          <td>Emil</td>
-          <td>Tobias</td>
-          <td>Linus</td>
-        </tr>
-      </table>
+      <InputImage />
     </div>
   );
 }
